9cbb4da8ff8a5995aa1f1452c096003e54f78a51
Shadi Barghash <shadolioware@gmail.com>
2018-03-25 20:00:54 +0200

    Complete postAnnouncement method

--- server/controllers/AnnouncementController.js
+++ server/controllers/AnnouncementController.js
@@ -1,20 +1,36 @@
+const _ = require('lodash');
 const mongoose = require('mongoose');
 Announcement = mongoose.model('Announcement');
 
 // GET /announcements
 module.exports.getAllAnnouncements = async (req, res) => {
 
-    res.send({
-        message: 'Hello Shadi'
+    Announcement.find().then((anns) => {
+        res.send({
+            announcements: anns
+        });
+    }).catch((err) => {
+        res.status(500).send({ err });
     });
 
 };
 
-// [TODO] POST /announcements
+// POST /announcements
 module.exports.postAnnouncement = async (req, res) => {
 
-    res.send({
-        message: 'Hello Shadi'
+    if(!req.body.title)
+        res.status(400).send('Announcement title is required');
+    if(!req.body.description)
+        res.status(400).send('Announcement description is required');
+
+    var announcement = new Announcement(_.pick(req.body, ['title', 'description']));
+    
+    announcement.save().then(() => {
+        res.send({
+            announcement
+        });
+    }).catch((err) => {
+        res.status(500).send({ err });
     });
 
 };
