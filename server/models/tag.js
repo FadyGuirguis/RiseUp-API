0f6c0aeaa0794350e45c9b4bb3b05f936e223a91
unknown <zeyadzaky@gmail.com>
2018-04-25 13:24:45 +0200

    test removeTag, cleanCode and documentation

--- server/models/tag.js
+++ server/models/tag.js
@@ -1,3 +1,13 @@
+/*H
+
+FILENAME: tag.js
+
+DESCRIPTION: This is the javascript file for the predefined tag schema.
+
+AUTHOR: Zeyad Zaky      START DATE: 26 March 2018.
+
+H*/
+
 const mongoose = require('mongoose');
 
 const TagSchema = mongoose.Schema({
