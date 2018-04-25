ce7ad9a8be453ded539522a95e6d52db4c8845f9
Shadi Barghash <shadolioware@gmail.com>
2018-04-25 14:03:57 +0200

    prepare users to be registered and used in saveOfficeHour tests

--- server/tests/OfficeHoursController.test.js
+++ server/tests/OfficeHoursController.test.js
@@ -3,6 +3,8 @@ base = process.env.PWD;
 const request = require('supertest');
 const expect = require('expect');
 var app = require("./../server").app;
+const {OfficeHour} = require('../models/OfficeHour');
+const {User} = require('../models/user');
 
 describe('Office Hours Controller',()=>{
 
@@ -26,11 +28,56 @@ describe('Office Hours Controller',()=>{
 
     describe('#saveOfficeHour',() => {
 
+        // Prepare 3 users to deal with them as experts through out this tests group
+        var expert1 = {
+            email: 'expert1@hosting.net',
+            password: 'creativePass',
+            profile: {
+                fullName: 'Expert One'
+            }
+        };
+
+        var expert2 = {
+            email: 'expert2@hosting.net',
+            password: 'creativePass',
+            profile: {
+                fullName: 'Expert Two'
+            }
+        };
+
+        var expert3 = {
+            email: 'expert3@hosting.net',
+            password: 'creativePass',
+            profile: {
+                fullName: 'Expert Three'
+            }
+        };
+
+        // Prepare 1 more user to use for sending most saveOfficeHour requests
+        var testUser = {
+            email: 'shadolio@shadolioware.net',
+            password: 'creativePass!!',
+            profile: {
+                fullName: 'Shadi Barghash'
+            }
+        };
+
         before((done) => {
-            // TODO: Remove all users from the DB
+
+            // Remove all users from the DB
+            User.remove({})
             // TODO: Add 3 'expert' users
+            .then(() => {
+
+            })
             // TODO: Add 1 other user to submit requests
-            done();
+            .then(() => {
+                done();
+            })
+            .catch((reason) => {
+                done(err);
+            });
+            
         });
 
         beforeEach((done) => {
@@ -39,6 +86,13 @@ describe('Office Hours Controller',()=>{
         });
 
         it('should not allow guests to submit officeHour requests', (done) => {
+
+            var body = {
+                title: 'office hour request by an expert',
+                description: 'testing that guests (not logged-in users) should not be allowed to make officeHour requests'
+                // TODO: experts:
+            };
+
             done();
         });
 
@@ -70,7 +124,7 @@ describe('Office Hours Controller',()=>{
             done();
         });
 
-        it('should save the request in the DB with the correct targeted expert and other fields', (done) => {
+        it('should save the request with status: pending, and correct expert and other fields', (done) => {
             done();
         });
 
