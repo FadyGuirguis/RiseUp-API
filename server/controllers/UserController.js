017d33ff7c6b6ae624c70e4d58342ad0bf3045bc
Anwar Labib <anwar.labib@student.guc.edu.eg>
2018-04-25 16:08:11 +0200

    Documentation of User

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,15 +1,38 @@
+/*H
+
+FILENAME: UserControleer.js
+
+DESCRIPTION: This is the javascript file for the UserController,
+             its main functionality is defining all method concerning the user (user - expert - admin)
+             
+FUNCTIONS:  
+           createUser
+           loginUser
+           editProfile
+           changePassword
+           logout
+           searchByName
+           getUserByID
+           getUserByToken
+
+AUTHOR: Daniel Achraf & Fady Sameh        START DATE: 3 Apr 2018.
+
+H*/
+
 const mongoose = require('mongoose');
 const _ = require('lodash');
 moment = require('moment'),
 User = mongoose.model('User');
 
-module.exports.createUser = async (req, res)=>{  //zabat el error messages, from MODEL nafso
+//createUser: This method is responsible for registering a new user and logging him in automatically returning his token.
+//He has to provide an unused email and a password of min length of 6 and a fullName name of min length of 6
+module.exports.createUser = async (req, res) => {
 
-  try{
+  try {
     var body = _.pick(req.body.user, ['email', 'password']);
     body.profile = {};
     body.profile.fullName = req.body.user.profile.fullName;
-  }catch(error){
+  } catch (error) {
     res.status(400).send('Please enter an email or password');
   }
 
@@ -19,16 +42,16 @@ module.exports.createUser = async (req, res)=>{  //zabat el error messages, from
     return user.generateAuthToken();
 
   }).then((token) => {
-    res.header('x-auth', token).send({user});
+    res.header('x-auth', token).send({ user });
   }).catch((e) => {
     if (e.name == 'ValidationError') {
-        if(typeof e.errors.email !== 'undefined'){
-            res.status(400).send(e.errors.email.message);
-        }else {
-          res.status(400).send(e);
-        }
+      if (typeof e.errors.email !== 'undefined') {
+        res.status(400).send(e.errors.email.message);
+      } else {
+        res.status(400).send(e);
+      }
 
-    }else {
+    } else {
       res.status(400).send(e);
     }
 
@@ -37,15 +60,17 @@ module.exports.createUser = async (req, res)=>{  //zabat el error messages, from
   });
 }
 
+//loginUser: This method is responsible for logging in a user and returning a token
+//He has to provide a valid email and password
 module.exports.loginUser = async (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
-  if(!body.email){
-    return res.status(400).send(); 
+  if (!body.email) {
+    return res.status(400).send();
   }
 
-  if(!body.password){
-    return res.status(400).send(); 
+  if (!body.password) {
+    return res.status(400).send();
   }
 
   User.findByCredentials(body.email, body.password).then((user) => {
@@ -53,51 +78,55 @@ module.exports.loginUser = async (req, res) => {
       return res.status(404).send();
     }
     return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send({user});
+      res.header('x-auth', token).send({ user });
     });
   }).catch((e) => {
-    if(e.message === 'email not found'){
-      res.status(404).send({msg : e.message});
+    if (e.message === 'email not found') {
+      res.status(404).send({ msg: e.message });
     }
-    if(e.message === 'password not correct'){
-      res.status(404).send({msg : e.message});
-    } else{
+    if (e.message === 'password not correct') {
+      res.status(404).send({ msg: e.message });
+    } else {
       res.status(500).send();
     }
   });
 };
 
+//editPassword: This method is responsible for editing any information about a user except password.
+//He has to provide the field he wants to edit with the new value 
 module.exports.editProfile = async (req, res) => {
- var id = req.user._id;
- var user = {};
- user.profile = req.user.profile
-  if(req.body.user && req.body.user.profile) {
+  var id = req.user._id;
+  var user = {};
+  user.profile = req.user.profile
+  if (req.body.user && req.body.user.profile) {
     if (req.body.user.profile.fullName || req.body.user.profile.description || req.body.user.profile.achievements) {
-        user.profile = _.merge(req.user.profile, req.body.user.profile);
+      user.profile = _.merge(req.user.profile, req.body.user.profile);
     } else if (req.body.user.profile.interests && _.isArray(req.body.user.profile.interests)) {
       user.profile.interests = req.body.user.profile.interests;
     }
     else if (req.body.user.profile.expertIn && _.isArray(req.body.user.profile.expertIn)) {
-    user.profile.expertIn = req.body.user.profile.expertIn;
-  }
+      user.profile.expertIn = req.body.user.profile.expertIn;
+    }
 
   }
-  User.findByIdAndUpdate(id, {$set:  user }, {new: true}).then((updatedUser) => {
-    if(!updatedUser) {
+  User.findByIdAndUpdate(id, { $set: user }, { new: true }).then((updatedUser) => {
+    if (!updatedUser) {
       return res.status(404).send();
     }
-    res.send({updatedUser});
+    res.send({ updatedUser });
   }).catch((e) => {
     res.status(500).send();
   })
-  }
+}
 
+//changePassword: This method is responsible for changing the password of the user
+//He has to provide the CORRECT old password and his new password which has to be min of 6 letters
 module.exports.changePassword = async (req, res) => {
   if (req.body.user && req.body.user.oldPassword && req.body.user.newPassword) {
     User.findByCredentials(req.user.email, req.body.user.oldPassword).then((user) => {
       user.password = req.body.user.newPassword;
       user.save().then((updatedUser) => {
-        res.send({updatedUser});
+        res.send({ updatedUser });
       }).catch((err) => {
         res.status(500).send();
       });
@@ -114,6 +143,8 @@ module.exports.changePassword = async (req, res) => {
 
 }
 
+//logout: This method is responsible for logging out the user.
+//He has to provide his token and it will be removed so he will be logged out
 module.exports.logout = async (req, res) => {
   User.findByToken(req.token).then((user) => {
     user.tokens = _.remove(user.tokens, (currentToken) => {
@@ -127,82 +158,74 @@ module.exports.logout = async (req, res) => {
   });
 }
 
+//searchByName: This method is responsible for searching for a user by his name
+//He has to provide a string and the users who have a full Name which CONTAINS this string will be returned 
+module.exports.searchByName = async (req, res) => {
 
-
-
-//search users by name,
-//msh exact name, like!,
-//example: there's Daniel Ashraf and Daniel Erian in the database,
-//I search for Daniel, yegeeli Daniel Ashraf w Daniel Erian
-
-//db.getCollection('users').find({"profile.fullName": {'$regex': 'tEst', '$options': 'i'}})
-
-
-  module.exports.searchByName = async (req, res) => {   //ALL
-
-    var query = mongoose.model('User').find({
-      _id: {
+  var query = mongoose.model('User').find({
+    _id: {
       $ne: req.user._id
-      },
-      "profile.fullName": {
+    },
+    "profile.fullName": {
       '$regex': req.body.name, '$options': 'i'
+    }
+  });
+
+  query.select("profile.fullName _id");
+
+  query.exec(function (error, result) {
+    if (error) {
+      res.status(500).send(error);
+    }
+    else {
+      if (result.length != 0) {
+        res.status(200).send({ result });
       }
-    });
-
-    query.select("profile.fullName _id");
-
-       query.exec(function(error, result){
-        if (error){
-          res.status(500).send(error); //something wrong happended
-        }
-        else{
-          if(result.length != 0){ //found an item
-              res.status(200).send({result});
-          }
-          else {
-              res.status(200).send({result});
-          }
-        }
-
-      });
-  }
-
-  ///user/:id
-  module.exports.getUserByID = async (req, res) => {
-
-     var query = mongoose.model('User').find({"_id":  req.params.id});
-
-     //select what you want, seperate by white space
-      query.select("email roles profile.fullName profile.description profile.rating profile.interests profile.expertIn profile.achievements");
-
-        query.exec(function(error, user){
-         if (error){
-           res.status(500).send(error); //something wrong happended
-         }
-         else{
-           if(user.length != 0){ //found an item
-               res.status(200).send({user: user[0]});
-           }
-           else {
-               res.status(404).send();
-           }
-         }
-
-       });
-  }
-
-
-  module.exports.getUserByToken = async (req,res)=>{
-    var token = req.header('x-auth');
-
-    var user = await User.findOne({
-      'tokens.token': token
-    });
-
-    if(User){
-      res.status(200).send({user});
+      else {
+        res.status(200).send({ result });
+      }
     }
-    else{
-      res.status(404).send();
+
+  });
+}
+
+//getUserById: This method is responsible for getting a user based on his ID
+//He has to provide the id and the user who has this id will be returned
+module.exports.getUserByID = async (req, res) => {
+
+  var query = mongoose.model('User').find({ "_id": req.params.id });
+
+  query.select("email roles profile.fullName profile.description profile.rating profile.interests profile.expertIn profile.achievements");
+
+  query.exec(function (error, user) {
+    if (error) {
+      res.status(500).send(error);
     }
+    else {
+      if (user.length != 0) {
+        res.status(200).send({ user: user[0] });
+      }
+      else {
+        res.status(404).send();
+      }
+    }
+
+  });
+}
+
+//getUserByToken: This method is responsible for getting a user based on his tokens
+//He has to provice a token and the user who has this token will be returned
+module.exports.getUserByToken = async (req, res) => {
+  var token = req.header('x-auth');
+
+  var user = await User.findOne({
+    'tokens.token': token
+  });
+
+  if (User) {
+    res.status(200).send({ user });
   }
+  else {
+    res.status(404).send();
+  }
+}
