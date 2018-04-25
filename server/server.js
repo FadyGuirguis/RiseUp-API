65ee34009c3e8ccfc365404992ab4062a9d9a8cc
Fady Sameh <fady97sameh@gmail.com>
2018-04-25 14:11:06 +0200

    added documentation

--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
+/*H
+
+FILENAME: server.js
+
+DESCRIPTION: This is the javascript file that loads all the files that include
+database schemas and the callback functions. It includes the routes and it
+serves the api on the current port.
+
+AUTHOR: Fady Sameh & Zeyad Zaky & Shady Ayman & Daniel Achraf
+& Abdelrahman Tarek
+
+START DATE: 25 March 2018.
+
+H*/
 //module imports
 var cors = require('cors');
 var express = require('express');
@@ -35,49 +49,99 @@ app.use(
   })
 );
 
+//parsing request bodies
 app.use(bodyParser.json());
 
-// User
+// User routes
 app.post('/register', asyncMiddleware(userController.createUser));
+
 app.post('/login', asyncMiddleware(userController.loginUser));
-app.post('/editProfile', authModule.authenticate, asyncMiddleware(userController.editProfile));
-app.post('/changePassword',  authModule.authenticate, asyncMiddleware(userController.changePassword));
-app.post('/logout', authModule.authenticate, asyncMiddleware(userController.logout));
-app.post('/searchByName',authModule.authenticate, asyncMiddleware(userController.searchByName));
-app.get('/user/:id',authModule.authenticate, asyncMiddleware(userController.getUserByID));
-app.get('/users/getByToken', authModule.authenticate, asyncMiddleware(userController.getUserByToken));
 
-// Tags
-app.post('/tag', authModule.authenticate, authModule.authAdmin,asyncMiddleware(tagController.addTag));
-app.get('/tags', authModule.authenticate, asyncMiddleware(tagController.getAllTags));
-app.delete('/tag/:id', authModule.authenticate, authModule.authAdmin, asyncMiddleware(tagController.removeTag));
+app.post('/editProfile', authModule.authenticate,
+asyncMiddleware(userController.editProfile));
 
-// Announcements
-app.get('/announcements', asyncMiddleware(announcementsCtlr.getAllAnnouncements));
-app.post('/announcement', authModule.authenticate, authModule.authAdmin, asyncMiddleware(announcementsCtlr.postAnnouncement));
-app.delete('/announcement/:id', authModule.authenticate, authModule.authAdmin, asyncMiddleware(announcementsCtlr.deleteAnnouncement));
+app.post('/changePassword',  authModule.authenticate,
+asyncMiddleware(userController.changePassword));
 
-// Office OfficeHour
-app.get('/officeHours',  authModule.authenticate, asyncMiddleware(OfficeHoursController.getOfficeHours));
-app.get('/officeHour/:id',  authModule.authenticate, asyncMiddleware(OfficeHoursController.getOfficeHour));
-app.post('/searchExperts', authModule.authenticate, asyncMiddleware(OfficeHoursController.getExperts));
-app.post('/officeHour', authModule.authenticate, asyncMiddleware(OfficeHoursController.saveOfficeHour));
-app.post('/acceptOfficeHour/:id',  authModule.authenticate, authModule.authExpert, asyncMiddleware(OfficeHoursController.acceptOfficeHour));
-app.post('/rejectOfficeHour/:id',  authModule.authenticate, authModule.authExpert, asyncMiddleware(OfficeHoursController.rejectOfficeHour));
-app.post('/confirmOfficeHour/:id',  authModule.authenticate, asyncMiddleware(OfficeHoursController.confirmOfficeHour));
+app.post('/logout', authModule.authenticate,
+asyncMiddleware(userController.logout));
 
-//Requests
-app.post('/request', authModule.authenticate, asyncMiddleware(requestController.addRequest));
-app.get('/requests', authModule.authenticate, authModule.authAdmin,asyncMiddleware(requestController.getAllRequests));
-app.post('/rejectRequest/:id', authModule.authenticate, authModule.authAdmin, asyncMiddleware(requestController.rejectRequest));
-app.post('/acceptRequest/:id', authModule.authenticate, authModule.authAdmin, asyncMiddleware(requestController.acceptRequest));
-app.post('/suspendExpert/:id', authModule.authenticate, authModule.authAdmin, asyncMiddleware(requestController.suspendExpert));
+app.post('/searchByName',authModule.authenticate,
+asyncMiddleware(userController.searchByName));
 
-// Reviews
-app.post('/review/:id', authModule.authenticate, asyncMiddleware(reviewsController.postReview));
-app.get('/reviews/:id', authModule.authenticate, authModule.authAdmin, asyncMiddleware(reviewsController.getReviewsOnUser));
+app.get('/user/:id',authModule.authenticate,
+asyncMiddleware(userController.getUserByID));
 
+app.get('/users/getByToken', authModule.authenticate,
+asyncMiddleware(userController.getUserByToken));
 
+// Tags routes
+app.post('/tag', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(tagController.addTag));
+
+app.get('/tags', authModule.authenticate,
+asyncMiddleware(tagController.getAllTags));
+
+app.delete('/tag/:id', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(tagController.removeTag));
+
+// Announcements routes
+app.get('/announcements',
+asyncMiddleware(announcementsCtlr.getAllAnnouncements));
+
+app.post('/announcement', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(announcementsCtlr.postAnnouncement));
+
+app.delete('/announcement/:id', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(announcementsCtlr.deleteAnnouncement));
+
+// Officehour routes
+app.get('/officeHours',  authModule.authenticate,
+asyncMiddleware(OfficeHoursController.getOfficeHours));
+
+app.get('/officeHour/:id',  authModule.authenticate,
+asyncMiddleware(OfficeHoursController.getOfficeHour));
+
+app.post('/searchExperts', authModule.authenticate,
+asyncMiddleware(OfficeHoursController.getExperts));
+
+app.post('/officeHour', authModule.authenticate,
+asyncMiddleware(OfficeHoursController.saveOfficeHour));
+
+app.post('/acceptOfficeHour/:id',  authModule.authenticate, authModule.authExpert,
+asyncMiddleware(OfficeHoursController.acceptOfficeHour));
+
+app.post('/rejectOfficeHour/:id',  authModule.authenticate, authModule.authExpert,
+asyncMiddleware(OfficeHoursController.rejectOfficeHour));
+
+app.post('/confirmOfficeHour/:id',  authModule.authenticate,
+asyncMiddleware(OfficeHoursController.confirmOfficeHour));
+
+//Requests routes
+app.post('/request', authModule.authenticate,
+asyncMiddleware(requestController.addRequest));
+
+app.get('/requests', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(requestController.getAllRequests));
+
+app.post('/rejectRequest/:id', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(requestController.rejectRequest));
+
+app.post('/acceptRequest/:id', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(requestController.acceptRequest));
+
+app.post('/suspendExpert/:id', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(requestController.suspendExpert));
+
+// Reviews routes
+app.post('/review/:id', authModule.authenticate,
+asyncMiddleware(reviewsController.postReview));
+
+app.get('/reviews/:id', authModule.authenticate, authModule.authAdmin,
+asyncMiddleware(reviewsController.getReviewsOnUser));
+
+/* selecting the port: 3000 when it's runnng locally. When the api is deployed,
+it uses the port saved in the Heroku environment*/
 const port = process.env.PORT || 3000;
 
 module.exports.app = app;
