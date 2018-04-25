af44ecab7a5af3610844c2a8c36363429c64fc7f
Shadi Barghash <shadolioware@gmail.com>
2018-04-25 12:42:34 +0200

    prepare header lines for saveOfficeHour tests

--- server/tests/OfficeHoursController.test.js
+++ server/tests/OfficeHoursController.test.js
@@ -24,8 +24,61 @@ describe('Office Hours Controller',()=>{
         });
     })
 
-    describe('#saveOfficeHour',()=>{
+    describe('#saveOfficeHour',() => {
+
+        before((done) => {
+            done();
+        });
+
+        beforeEach((done) => {
+            done();
+        });
+
+        it('should not allow guests to submit officeHour requests', (done) => {
+            done();
+        });
+
+        it('should not allow requests targeting 0 experts', (done) => {
+            done();
+        });
+
+        it('should not allow requests targeting any number of experts > 3', (done) => {
+            done();
+        });
+
+        it('should not allow requests with no title', (done) => {
+            done();
+        });
+
+        it('should not allow requests with no description', (done) => {
+            done();
+        });
+
+        it('should not allow requests with no title and no description', (done) => {
+            done();
+        });
+
+        it('should save in the DB one officeHour request for each targeted expert (3 experts)', (done) => {
+            done();
+        });
+
+        it('should save in the DB one officeHour request for each targeted expert (2 experts)', (done) => {
+            done();
+        });
+
+        it('should save the request in the DB with the correct targeted expert and other fields', (done) => {
+            done();
+        });
+
+        it('should save only one request for each expert (no duplicates)', (done) => {
+            done();
+        });
+
         it('should pass',(done)=>{
+            done();
+        });
+
+        after((done) => {
             done();
         });
     })
