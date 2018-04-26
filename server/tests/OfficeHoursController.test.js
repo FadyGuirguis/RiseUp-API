e8c54f6a4965d2f7cf1e4f720ac3e23cf4005e71
Amr Saadi <amr_saadi@hotmail.com>
2018-04-26 21:10:49 +0200

    rejectOfficeHour testing complete

--- server/tests/OfficeHoursController.test.js
+++ server/tests/OfficeHoursController.test.js
@@ -239,9 +239,394 @@ describe('Office Hours Controller',()=>{
     })
 
     describe('#rejectOfficeHour',()=>{
-        it('should pass',(done)=>{
+
+    //Creating all the users needed for the test cases
+    var user = {
+        email : 'user@something.com',
+        password : 'user1234',
+        profile : {
+            "fullName" : "User Awi"
+        }
+    };
+
+    var expert = {
+        email : 'expert@something.com',
+        password : 'expert',
+        profile : {
+            "fullName" : "Expert Awi"
+        }
+    };
+
+    var expert2 = {
+        email : 'expert2@something.com',
+        password : 'expert2',
+        profile : {
+            "fullName" : "Expert Ghareeb"
+        }
+    };
+
+    var RUser ={
+        email : 'user@something.com',
+        password : 'user1234',
+        profile : {
+            "fullName" : "User Awi"
+        }
+    };
+
+    var RExpert ={
+        email : 'expert@something.com',
+        password : 'expert',
+        profile : {
+            "fullName" : "Expert Awi"
+        }
+    };
+
+    var RExpert2 ={
+        email : 'expert2@something.com',
+        password : 'expert2',
+        profile : {
+            "fullName" : "Expert Ghareeb"
+        }
+    };
+
+    //Before Block, clearing all users and registering the users and experts created above
+    before((done) => {
+
+        User.remove({}).then(() => {
+            return request(app).post("/register").send({user: user}).expect(200);
+        }).then((res) => {
+            RUser = res.body.user;
+        }).then(() => {
+            return request(app).post("/register").send({user: expert}).expect(200);
+        }).then((res) => {
+            RExpert = res.body.user;
+            return User.findByIdAndUpdate(RExpert._id, { $push: { roles: 'expert' } }, { new: true });
+        }).then(() => {
+            return request(app).post("/register").send({user: expert2}).expect(200);
+        }).then((res) => {
+            RExpert2 = res.body.user;
+            return User.findByIdAndUpdate(RExpert2._id, { $push: { roles: 'expert' } }, { new: true });
+        }).then(() => {
             done();
+        })
+        .catch((reason) => {
+            console.log(reason);
+            done(err);
         });
+    });
+
+    //Removing all office hours before each test
+    beforeEach((done) => {
+        OfficeHours.remove({}).then(() => {
+            done();
+        }).catch((reason) => {
+            console.log(reason);
+            done(reason);
+        });
+    });
+
+
+    /*Testing if an expert can reject an office hours request sent to him*/
+    it('should reject Office Hour request',(done)=>{
+
+        var officehour ={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title: 'Office hours 1',
+              description: 'first office hours',
+              status: 'pending',
+              lastModified: new Date("2018-01-01")
+        }
+
+        body={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title:  'Office hours 1',
+              description: 'first office hours',
+              status: 'pending',
+              lastModified: new Date("2015-03-25")
+
+        } 
+        body=new OfficeHours(body);
+        officehour = new OfficeHours(officehour);
+        officehour.save().then(()=>{body._id=officehour._id;
+            request(app).post('/rejectOfficeHour/'+body._id+'')
+            .set({'x-auth': RExpert.tokens[0].token}).send({officeHour:body}).expect(200)
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            return done();
+        });});
+
+    });
+
+
+    /*Testing expert should not reject a request that has already been rejected by having an
+    expert try to reject a request with status 'rejected'*/
+    it('should not reject an already rejected Office Hour request',(done)=>{
+        var officehour ={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title: 'Office hours 1',
+              description: 'first office hours',
+              status: 'rejected',
+              lastModified: new Date("2018-01-01")
+        }
+
+        body={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title:  'Office hours 1',
+              description: 'first office hours',
+              status: 'rejected',
+              lastModified: new Date("2015-03-25")
+
+        } 
+        body=new OfficeHours(body);
+        officehour = new OfficeHours(officehour);
+        officehour.save().then(()=>{body._id=officehour._id;
+            request(app).post('/rejectOfficeHour/'+body._id+'')
+            .set({'x-auth': RExpert.tokens[0].token}).send({officeHour:body}).expect(400).expect((res)=>{
+                expect(res.res.text).toBe("{\"err\":\"This office hour has already been replied to\"}")
+            })
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            return done();
+        });});
+
+    });
+
+
+    /*Testing expert should not reject a request that has already been rejected by having an
+    expert try to reject a request with status 'accepted'*/
+    it('should not reject an accepted Office Hour request',(done)=>{
+        var officehour ={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title: 'Office hours 1',
+              description: 'first office hours',
+              status: 'accepted',
+              lastModified: new Date("2018-01-01")
+        }
+
+        body={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title:  'Office hours 1',
+              description: 'first office hours',
+              status: 'accepted',
+              lastModified: new Date("2015-03-25")
+
+        } 
+        body=new OfficeHours(body);
+        officehour = new OfficeHours(officehour);
+        officehour.save().then(()=>{body._id=officehour._id;
+            request(app).post('/rejectOfficeHour/'+body._id+'')
+            .set({'x-auth': RExpert.tokens[0].token}).send({officeHour:body}).expect(400).expect((res)=>{
+                expect(res.res.text).toBe("{\"err\":\"This office hour has already been replied to\"}")
+            })
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            return done();
+        });});
+
+    });
+
+
+    /*Testing expert should not reject a request sent to another expert by creating an office hour request
+    between a user and ann expert and then trying to reject it by a different expert*/
+    it('should not reject an Office Hour request sent to another expert',(done)=>{
+        var officehour ={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              /*expert2: {
+                //  _id: RExpert2._id,
+                //  name: 'Expert Ghareeb'
+              },
+              */
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title: 'Office hours 1',
+              description: 'first office hours',
+              status: 'pending',
+              lastModified: new Date("2018-01-01")
+        }
+
+        body={
+            user: {
+                _id:RUser._id,
+                name: 'User Awi'
+              },
+
+              expert: {
+                _id: RExpert._id,
+                name: 'Expert Awi'
+              },
+              isUserReviewed: true,
+              isExpertReviewed: true,
+              createdOn: new Date("2018-01-01"),
+              suggestedSlots: {
+                slots: null,
+                createdOn: null
+              },
+              chosenSlot: {
+                slot: null,
+                createdOn: null
+              },
+              title:  'Office hours 1',
+              description: 'first office hours',
+              status: 'pending',
+              lastModified: new Date("2015-03-25")
+
+        } 
+        body=new OfficeHours(body);
+        officehour = new OfficeHours(officehour);
+        officehour.save().then(()=>{body._id=officehour._id;
+            request(app).post('/rejectOfficeHour/'+body._id+'')
+            .set({'x-auth': RExpert2.tokens[0].token}).send({officeHour:body}).expect(400).expect((res)=>{
+                expect(res.res.text).toBe("{\"err\":\"This request has not been sent to you\"}")
+            })
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            return done();
+        });});
+    });
+
+    //Removing everything
+    after((done)=>{
+        User.remove({}).then(()=>{
+        OfficeHours.remove({})}).then(()=>{
+            done();
+        })
+    });
+
     })
 
     describe('#confirmOfficeHour',()=>{
