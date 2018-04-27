74b8fbaa4dfdf354dbce3538d7f1dfd2c3854483
Anwar Labib <anwar.labib@student.guc.edu.eg>
2018-04-27 23:48:43 +0200

    Fix some minor bugs

--- server/tests/AnnouncementController.test.js
+++ server/tests/AnnouncementController.test.js
@@ -8,153 +8,54 @@ const { User } = require('../models/user');
 const { Announcement } = require('../models/Announcement');
 describe('Announcement Controller', () => {
 
-    describe('#getAllAnnouncements',()=>{
+    describe('#getAllAnnouncements', () => {
+        var ann;
         beforeEach((done) => {
-            
-            User.remove({}).then((err, res) => {
-                var user = {
-                    email: 'nothing@something.com',
-                    password: 'something',
-                    profile: {
-                        "fullName": "Nothing Something"
-                    }
-                };
-                 
-                request(app)
-                    .post("/register")
-                    .send({user})
-                    .expect(200)
-                    .then((res)=>{
-                        userr = res.body.user;
-                        user = {
-                            email : 'expert@something.com',
-                            password : 'something',
-                            profile : {
-                                "fullName" : "expert Something"
-                            }
-                        };
-
-                        request(app)
-                            .post("/register")
-                            .send({user})
-                            .expect(200)
-                            .then((res)=>{
-                                expertt = res.body.user;
-                                Announcement = new Announcement({
-                                    user : {
-                                        _id : userr._id,
-                                        name : userr.profile.fullName
-                                    } ,
-                                    expert : {
-                                        _id : expertt._id,
-                                        name : expertt.profile.fullName
-                                    },
-
-                                    title : "ask expert ",
-
-                                    description : "about nothing",
-
-                                });
-
-                        Announcement.save().then((doc)=>{
-                            Announcement = doc ;
-                            done();
-                        })
-                            .catch((err)=>{
-                                console.log(err);
-                            });
-                    });
-
-            });
-        
+            Announcement.remove().then(()=>{
+                ann = new Announcement({
+                    title: "ask expert ",
+                    description: "about nothing"
+                });
+    
+                ann.save().then((doc) => {
+                    ann = doc;
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            }).catch((err)=>{
+                done(err);
+            })
+        });
 
 
-            it('should announcement be found using user token ', (done) => {
-                request(app)
-                    .get('/announcement/'+Announcement._id)
-                    .expect(200)
-                    .set({
-                        'x-auth' : userr.tokens[0].token
-                    })
-                    .end((err,res)=>{
-                        if(err)
-                            done(err);
-                        else{
-                            done();
-                        }
-                    });
-            });
-        it('should announcement be found using expert token ', (done) => {
+        it('Anyone can see the announcements (No token needed)', (done) => {
             request(app)
-                .get('/announcement/'+Announcement._id)
+                .get('/announcements')
                 .expect(200)
-                .set({
-                    'x-auth' : expertt.tokens[0].token
-                })
-                .end((err,res)=>{
-                    if(err)
+                .end((err, res) => {
+                    if (err)
                         done(err);
-                    else{
+                    else {
+                        expect(res.body.announcements.length).toBe(1);
                         done();
                     }
                 });
         });
 
-
-        it(' Should be not found ', (done) => {
-            request(app)
-                .get('/announcement/')
-                .expect(404)
-                .end((err,res)=>{
-                    if(err)
-                        done(err);
-                    else{
-                        done();
-                    }
-                });
-        });
-        it(' Should be unauthorized', (done) => {
-            request(app)
-                .get('/announcement/'+Announcement._id)
-                .expect(401)
-                .end((err,res)=>{
-                    if(err)
-                        done(err);
-                    else{
-                        done();
-                    }
-                });
-        });
-        it(' Should be incorrect as no announcement hour with that id', (done) => {
-            request(app)
-                .get('/announcement/'+35131)
-                .set({
-                    'x-auth' : userr.tokens[0].token
-                })
-                .expect(500)
-                .end((err,res)=>{
-                    if(err)
-                        done(err);
-                    else{
-                        done();
-                    }
-                });
-        });
-       
-    });
-});
 
 
         after((done) => {
-            Announcement.remove({}).then((res)=>{
-                User.remove({}).then((res)=>{
+            Announcement.remove({}).then((res) => {
+                User.remove({}).then((res) => {
                     done();
                 })
 
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err);
             })
-            });
+        });
 
 
     })
@@ -240,77 +141,77 @@ describe('Announcement Controller', () => {
         })
     });
 
-        describe('#deleteAnnouncement', () => {
-            var admin = {
-                email: 'admin@admin.com',
-                password: 'something',
-                profile: {
-                    "fullName": "Admin Admin"
-                }
-            };
-
-            var user = {
-                email: 'user@user.com',
-                password: 'something',
-                profile: {
-                    "fullName": "User User"
-                }
+    describe('#deleteAnnouncement', () => {
+        var admin = {
+            email: 'admin@admin.com',
+            password: 'something',
+            profile: {
+                "fullName": "Admin Admin"
             }
+        };
 
-            var an = {
-                title: 'title',
-                description: 'description'
+        var user = {
+            email: 'user@user.com',
+            password: 'something',
+            profile: {
+                "fullName": "User User"
             }
-            before((done) => {
-                User.remove({}).then(() => {
-                    request(app).post('/register').send({ user: admin }).expect(200).end((err, res) => {
-                        if (err) {
-                            return done(err);
-                        }
-                        admin = res.body.user;
-                        User.findByIdAndUpdate(res.body.user._id, { roles: ['admin'] }, { new: true }).then(() => {
-                            Announcement.remove({}).then(() => {
-                                an = new Announcement(an);
-                                an.save().then(() => {
+        }
 
-                                    request(app).post('/register').send({ user: user }).expect(200).end((err, res) => {
-                                        if (err) {
-                                            return done(err);
-                                        }
-                                        user = res.body.user;
-                                        User.findByIdAndUpdate(res.body.user._id, { roles: ['user'] }, { new: true }).then(() => {
-                                            return done();
-                                        })
+        var an = {
+            title: 'title',
+            description: 'description'
+        }
+        before((done) => {
+            User.remove({}).then(() => {
+                request(app).post('/register').send({ user: admin }).expect(200).end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    admin = res.body.user;
+                    User.findByIdAndUpdate(res.body.user._id, { roles: ['admin'] }, { new: true }).then(() => {
+                        Announcement.remove({}).then(() => {
+                            an = new Announcement(an);
+                            an.save().then(() => {
+
+                                request(app).post('/register').send({ user: user }).expect(200).end((err, res) => {
+                                    if (err) {
+                                        return done(err);
+                                    }
+                                    user = res.body.user;
+                                    User.findByIdAndUpdate(res.body.user._id, { roles: ['user'] }, { new: true }).then(() => {
+                                        return done();
                                     })
                                 })
                             })
                         })
                     })
                 })
-            });
+            })
+        });
 
-            it('An Admin can delete an announcement', (done) => {
-                Announcement.find({}).then((ans) => {
-                    request(app).delete('/announcement/' + ans[0]._id + '').set({ 'x-auth': admin.tokens[0].token }).expect(200).end((err, res) => {
-                        //console.info(res.status+' '+res.res.text);   
-                        if (err) {
-                            return done(err);
-                        }
-                        return done();
-                    })
+        it('An Admin can delete an announcement', (done) => {
+            Announcement.find({}).then((ans) => {
+                request(app).delete('/announcement/' + ans[0]._id + '').set({ 'x-auth': admin.tokens[0].token }).expect(200).end((err, res) => {
+                    //console.info(res.status+' '+res.res.text);   
+                    if (err) {
+                        return done(err);
+                    }
+                    return done();
                 })
-            });
+            })
+        });
 
-            it('A non-admin cant delete an anouncement', (done) => {
-                Announcement.find({}).then((ans) => {
-                    request(app).delete('/announcement/' + 'sdklnas7y').set({ 'x-auth': user.tokens[0].token }).expect(403).end((err, res) => {
-                        //console.info(res.status+' '+res.res.text);   
-                        if (err) {
-                            return done(err);
-                        }
-                        return done();
-                    })
+        it('A non-admin cant delete an anouncement', (done) => {
+            Announcement.find({}).then((ans) => {
+                request(app).delete('/announcement/' + 'sdklnas7y').set({ 'x-auth': user.tokens[0].token }).expect(403).end((err, res) => {
+                    //console.info(res.status+' '+res.res.text);   
+                    if (err) {
+                        return done(err);
+                    }
+                    return done();
                 })
-            });
-        })
+            })
+        });
+    })
 })
