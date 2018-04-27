e2b0792696e5d9f2ccdadf87b445a665b994f10e
MHGanainy <mohamedhelganainy@gmail.com>
2018-04-27 14:30:24 +0200

    Testing the Method AcceptRequest

--- server/tests/RequestController.test.js
+++ server/tests/RequestController.test.js
@@ -193,9 +193,126 @@ describe('Request Controller',()=>{
     })
 
     describe('#acceptRequest',()=>{
-        it('should pass',(done)=>{
-            done();
-        });
+        describe('#acceptRequest', () => {
+            //-------------------------Declaration----------------------------------
+            var admin = {
+                email: 'admin@admin.com',
+                password: 'something',
+                profile: {
+                    "fullName": "Admin Admin"
+                }
+            };
+            var user = {
+                email: 'Mohamed@admin.com',
+                password: 'something',
+                profile: {
+                    "fullName": "Mohamed Hesham"
+                }
+            };
+
+            var ResUser = {
+                email: 'mostafa@something.com',
+                password: 'something',
+                profile: {
+                    "fullName": "Mostafa Amer"
+                }
+            };
+            var ResAdmin = {
+                email: 'Joe@something.com',
+                password: 'something',
+                profile: {
+                    "fullName": "Joe salah"
+                }
+            };
+            var req = {
+                description: "Testing Descp"
+            };
+
+            beforeEach((done) => {
+                User.remove({}).then(() => {
+                    request(app).post('/register').send({ user: user }).expect(200).end((err, res) => {
+                        //console.info(res.status + ' ' + res.res.text);
+                        if (err) {
+                            return done(err);
+                        }
+                        ResUser = res.body.user;
+                        request(app).post('/register').send({ user: admin }).expect(200).end((err, res) => {
+                            if (err) {
+                                return done(err);
+                            }
+                            ResAdmin = res.body.user;
+                            User.findByIdAndUpdate(ResAdmin._id, { roles: ['admin'] }, { new: true }).then(() => {
+                                Request.remove({}).then(() => {
+                                    return done();
+                                });
+                            })
+                        });
+                    });
+                });
+            });
+            it('Succeed', (done) => {
+                request(app).post('/request').set({ 'x-auth': ResUser.tokens[0].token }).send({ request: req }).expect(200).end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    Request.find({}).then((res) => {
+                        //------------------------------------------------------------
+                        request(app).post('/acceptRequest/' + res[0]._id + '').set({ 'x-auth': ResAdmin.tokens[0].token }).send(res[0]).expect(200).end((err, res) => {
+                            //console.info(res.status);
+                            if (err) {
+                                return done(err);
+                            }
+                            return done();
+                        });
+                        //------------------------------------------------------------
+
+                    });
+                });
+            });
+            //-------------------------Pending Test---------------------------------
+            it('shouldnot be able to reject a not pending Request ', (done) => {
+                request(app).post('/request').set({ 'x-auth': ResUser.tokens[0].token }).send({ request: req }).expect(200).end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    Request.find({}).then((res) => {
+                        //------------------------------------------------------------
+                        var Resreq = res[0];
+                        Resreq.status = 'accepted';
+                        Resreq.save().then(() => {
+                            Request.find({}).then((res) => {
+                                //----------------------------------------------------
+                                request(app).post('/acceptRequest/' + res[0]._id + '').set({ 'x-auth': ResAdmin.tokens[0].token }).send(res[0]).expect(400).end((err, res) => {
+                                    console.info(res.status + ' ' + res.res.text);
+                                    if (err) {
+                                        return done(err);
+                                    }
+                                    //expect(res.res.text).toBe('This request is already evaluated');
+                                    return done();
+                                });
+
+                                //----------------------------------------------------
+                            });
+                        })
+                        //------------------------------------------------------------
+
+                    });
+                });
+            });
+            //-----------------------------No Request with Such ID--------------------------------
+            it('Remove Request without User', (done) => {
+                var dummyRequestId = ResUser._id;
+                request(app).post('/acceptRequest/' + dummyRequestId + '').set({ 'x-auth': ResAdmin.tokens[0].token }).send(req).expect(404).end((err, res) => {
+                    console.info(res.status + ' ' + res.res.text);
+                    if (err) {
+                        return done(err);
+                    }
+                    expect(res.res.text).toBe('There is no request available with such id');
+                    return done();
+                });
+            });
+        })
+
     })
 
     describe('#suspendExpert',()=>{
